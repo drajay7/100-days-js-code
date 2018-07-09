2b2b580515ffb99640ef85236443c7c7548eecc4
drajay7 <40847584+drajay7@users.noreply.github.com>
2018-07-09 13:02:06 +0530

    Day 2 Strings

---
+++ code/javascript/day-1/string.js
@@ -0,0 +1,9 @@
+var sentence1 = "He \n said \"Hi!\"";
+var sentence2 = 'He Said \n "\Hi!"';
+console.log(sentence1);
+console.log(sentence2);
+var myname= "Dr." + 'Ajay';
+console.log(myname);
+console.log("my name is" + myname);
+myname+= " is My Name";
+console.log(myname);
