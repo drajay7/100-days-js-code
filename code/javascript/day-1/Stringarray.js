6fd30939e3d28d40d57359f72bcdffefb578a25e
drajay7 <40847584+drajay7@users.noreply.github.com>
2018-07-12 13:30:57 +0530

    Day2

---
+++ code/javascript/day-1/Stringarray.js
@@ -0,0 +1,7 @@
+var name = "Ajay";
+console.log(name[0]);
+console.log(name[name.length - 1]);
+console.log(name[name.length - 2]);
+console.log(name[name.length - 3]);
+name = "Vijay";
+console.log(name);
