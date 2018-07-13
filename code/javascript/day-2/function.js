5f8578d7fc9c4711676950e7331dba0a06122710
drajay7 <40847584+drajay7@users.noreply.github.com>
2018-07-13 14:17:34 +0530

    day 3

---
+++ code/javascript/day-2/function.js
@@ -0,0 +1,18 @@
+function square (num)
+{
+return (num*num);
+
+}
+var n1=5;
+//n1 = prompt("Pls enter the number");
+console.log(square(n1));
+var n1=5, n2=3;
+function addsquare(n1, n2)
+{
+function s(n1){
+  return n1*n1;
+}
+return s(n1)+s(n2);
+}
+
+console.log(addsquare(n1,n2));
